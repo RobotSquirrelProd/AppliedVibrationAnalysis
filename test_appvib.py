corrected reference in nx calculation

test_appvib.py
@@ -344,6 +344,7 @@ class TestClSig(TestCase):
 
         # Signal feature class test for nx plots
         class_test_sig_features = appvib.ClSigFeatures(self.np_test_trigger, self.d_fs_test_trigger)
+        class_test_sig_features.plt_nx()
         d_eventtimes_sig = class_test_sig_features.np_d_est_triggers(np_d_sig=class_test_sig_features.np_d_sig,
                                                                      i_direction=self.i_direction_test_trigger_rising,
                                                                      d_threshold=self.d_threshold_test_trigger,
@@ -357,15 +358,16 @@ class TestClSig(TestCase):
                                                                      d_threshold=self.d_threshold_test_trigger,
                                                                      d_hysteresis=self.d_hysteresis_test_trigger,
                                                                      b_verbose=False)
+        # This sequence caught mis-management of plot titles.
         class_test_sig_features.plt_nx()
         class_test_sig_features.plt_nx()
-        class_test_sig_features.plt_nx(str_plot_nx_desc='test_plt_nx ClSigFeatures Implicit call')
+        class_test_sig_features.plt_nx(str_plot_desc='test_plt_nx ClSigFeatures Implicit call')
 
         np_d_nx_sig = class_test_sig_features.calc_nx(np_d_sig=class_test_sig_features.np_d_sig,
                                                       np_d_eventtimes=d_eventtimes_sig,
                                                       b_verbose=False, idx=0)
         self.assertAlmostEqual(np.abs(np_d_nx_sig[0]), self.d_test_trigger_amp, 2)
-        class_test_sig_features.plt_nx(str_plot_nx_desc='test_plt_nx ClSigFeatures Explicit call')
+        class_test_sig_features.plt_nx(str_plot_desc='test_plt_nx ClSigFeatures Explicit call')
 
     def test_plt_apht(self):
 